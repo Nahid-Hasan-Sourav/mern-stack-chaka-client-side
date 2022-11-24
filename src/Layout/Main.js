6ef02ea203ac6fbd3f9b7df03abc2a174c0f211c
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-11-24 22:22:44 +0600

    install all necessary packages and create the project structure and this is my first commit

---
+++ src/Layout/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
+
+
+
+const Main = () => {
+    return (
+        <div>
+            <div>
+            <Navbar></Navbar>
+            </div>
+            <div>
+            <Outlet></Outlet>
+            </div>
+            <div>
+            <Footer></Footer>
+            </div>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            This is Banner Page
+        </div>
+    );
+};
+
+export default Banner;
